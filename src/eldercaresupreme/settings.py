f2ff41666d1025bead4914984183e15b0b03aa0a
Tobiasz <tobiasz0889@gmail.com>
2020-07-02 15:56:49 +0100

    Update requirements load dotenv in setting and setup base.html

--- src/eldercaresupreme/settings.py
+++ src/eldercaresupreme/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dotenv
+
+# Load dotenv
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
