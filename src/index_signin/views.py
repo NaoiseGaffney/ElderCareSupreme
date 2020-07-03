87d3a32df1841da55e402981021881bc40169c00
Tobiasz <tobiasz0889@gmail.com>
2020-07-03 12:24:41 +0100

    Add login authentycation and set up messages

--- src/index_signin/views.py
+++ src/index_signin/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate
 from django.views.generic import View
 
 # Create your views here.
 class IndexSignInView(View):
+    template_view = "index.html"
     def post(self, request, *args, **kwargs):
         username = request.POST['username']
         password = request.POST['password']
@@ -11,6 +14,11 @@ class IndexSignInView(View):
 
         if user is not None:
             login(request, user)
+            messages.success(request, f'Welcome back {username}!')
             return redirect('dashboard/dash.html')
         else:
-            
+            messages.error(request, 'The username or password is incorrect')
+            return render(request, self.template_view, {})
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_view, {})
