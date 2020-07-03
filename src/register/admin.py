8454e90015888a8b6dc36af9b13bae3a2094fa21
Tobiasz <tobiasz0889@gmail.com>
2020-07-03 13:05:00 +0100

    Create a new models for user profile and user role

--- src/register/admin.py
+++ src/register/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserRole, UserProfile
 
 # Register your models here.
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'role')
+    list_display_links = ('id', 'role')
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_name', 'post_code', 'role', 'city')
+    list_display_links = ('id', 'user_name')
+
+admin.site.register(UserRole, UserRoleAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- src/register/froms.py
+++ src/register/froms.py
@@ -2,13 +2,18 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
+from .models import UserProfile
 
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
-    phone = forms.IntegerField(required=True)
+
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email', 'phone', 'password1', 'password2')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
