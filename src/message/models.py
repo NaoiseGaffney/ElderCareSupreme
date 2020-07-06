31b4c1059f4e383b45569736f1a9a7253fe2e610
Tobiasz <tobiasz0889@gmail.com>
2020-07-06 18:01:57 +0100

    Change model to date-time, display date and time to post and fix dipslay of post from oldest to newest

--- src/message/models.py
+++ src/message/models.py
@@ -8,8 +8,8 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="Anonymouse")
     task = models.ForeignKey(Task, on_delete=models.CASCADE, blank=False, null=False)
     message = models.CharField(max_length=300, blank=False, null=False)
-    date_created = models.DateField(auto_now_add=False, default=timezone.now)
-    date_edited = models.DateField(auto_now_add=timezone.now)
+    date_created = models.DateTimeField(auto_now_add=False, default=timezone.now)
+    date_edited = models.DateTimeField(auto_now_add=timezone.now)
 
     def __str__(self):
         return f'{self.user} + {self.task}'
