3592662425f4f0a46aa1898048c1425ae90a1f53
Tobiasz <tobiasz0889@gmail.com>
2020-07-07 11:15:49 +0100

    Add dynamic template for message card display

--- src/message/views.py
+++ src/message/views.py
@@ -21,11 +21,16 @@ class MessageView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         id_ = self.kwargs.get("pk")
+        # Get task and slice to one, prevent error when None
         task = Task.objects.filter(id=id_)[:1]
         user = self.request.user
         message = self.request.POST.get('chat-message', None)
         if message is not None:
             Message.objects.create(user=user, task=task[0], message=message)
+            # Increment by one message count
+            if task:
+                task[0].message_count += 1
+                task[0].save()
         get_messages = Message.objects.filter(task=id_).order_by('date_created')
         context = {
             'user': user,
